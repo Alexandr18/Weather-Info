f0e894406985908d72b11ba28e5a5e391d040a5f
Alex <33393952+Alexandr18@users.noreply.github.com>
2018-11-25 22:20:03 +0500

    Initial commit

---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render() {
+      return (
+        <div>Компонент с погодной инфой</div>
+      );
+    }
+}
+
+export default Weather;
